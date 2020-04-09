17682ac979b854cd55fc0d3d71e09feec971d8ac
Pedro Gomes <pedrompg311@gmail.com>
2020-04-09 16:41:35 +0100

    Exported scss to it's own dir

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,7 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
-
-<style>
-.container {
-  display: grid;
-  grid-template-columns: repeat(12, [col-start] 1fr);
-  grid-column-gap: 1rem;
-  margin: 0 auto;
-  width: 90%;
-}
-
-.full-width-background {
-  width: 100vw;
-  position: relative;
-  left: 50%;
-  right: 50%;
-  margin-left: -50vw;
-  margin-right: -50vw;
-}
-</style>

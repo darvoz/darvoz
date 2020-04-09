0875e97a4598aa559246849bb8b416e5d75978bb
Tiago Rechau <tiago.rechau@mercedes-benz.io>
2020-04-09 18:55:32 +0100

    Add Button Component

---
+++ pages/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Button>Primary Button</Button>
+    <Button kind="secondary">Secondary Button</Button>
+    <Button kind="tertiary">Simple Link</Button>
+  </div>
+</template>
+
+<script>
+import Button from '~/components/Button/Button.vue'
+export default {
+  components: {
+    Button
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../styles/_global';
+</style>

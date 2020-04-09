c389a457defd76daaa6fc148bb52110bfd62d707
Pedro Gomes <pedrompg311@gmail.com>
2020-04-09 22:55:39 +0100

    Added initial structure for the FAQ section

---
+++ components/FaqSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container faq-section"></div>
+</template>
+
+<script>
+export default {
+  name: 'FaqSection'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/_global.scss';
+</style>

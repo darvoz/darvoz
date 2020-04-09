f4612ed23586c74357dc339c98f7416cc90fbf07
Tiago Rechau <tiago.rechau@mercedes-benz.io>
2020-04-10 00:09:05 +0100

    Add navigation bar and background-color

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <how-it-works></how-it-works>
-    <about></about>
+  <div class="main">
+    <navigation-bar />
+    <how-it-works />
+    <about id="o-que-e" />
   </div>
 </template>
 
 <script>
-import HowItWorks from '~/components/HowItWorks.vue'
-import About from '~/components/About.vue'
+import NavigationBar from '~/components/NavigationBar/NavigationBar'
+import HowItWorks from '~/components/HowItWorks'
+import About from '~/components/About'
 
 export default {
   components: {
+    NavigationBar,
     HowItWorks,
     About
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../styles/_global.scss';
+.main {
+  background-color: $smoke-white;
+}
+</style>

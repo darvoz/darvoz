57319ed2b7698eff171e1f2b54ff1c6c3014b1f8
Tiago Rechau <tiago.rechau@mercedes-benz.io>
2020-04-09 19:38:11 +0100

    Add Accordion Component

--- pages/test.vue
+++ pages/test.vue
@@ -3,14 +3,23 @@
     <Button>Primary Button</Button>
     <Button kind="secondary">Secondary Button</Button>
     <Button kind="tertiary">Simple Link</Button>
+    <Accordion title="Quais são as empresas participantes deste projeto?">
+      Tempor venenatis semper sed elit nisl imperdiet enim euismod luctus.
+      Volutpat dignissim duis vel, sed lectus nec luctus. Duis diam vestibulum
+      in tempor, ridiculus tempor fringilla sociis. Dictum eget velit placerat
+      nulla habitant tincidunt hendrerit. Vel varius hendrerit eleifend enim
+      blandit est maecenas. Ut cras sed posuere consectetur erat eleifend.
+    </Accordion>
   </div>
 </template>
 
 <script>
 import Button from '~/components/Button/Button.vue'
+import Accordion from '~/components/Accordion/Accordion.vue'
 export default {
   components: {
-    Button
+    Button,
+    Accordion
   }
 }
 </script>

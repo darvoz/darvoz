95d8806c4b41d4df69754fc0d37ebc08e6435fa8
Pedro Gomes <pedrompg311@gmail.com>
2020-04-11 18:04:35 +0100

    Added accessibility and more mobile stuff to the intro section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <navigation-bar></navigation-bar>
-    <intro-section />
+    <intro-section :margin="'80px'" />
     <stats-section />
     <about-section id="o-que-e" />
     <how-to-donate />

bcbccb133551a6eed8646689ec67dc4a1299b664
Tiago Rechau <tiago.rechau@mercedes-benz.io>
2020-04-11 22:34:12 +0100

    How to donate slides

--- components/HowToDonateSection/HowToDonateSection.vue
+++ components/HowToDonateSection/HowToDonateSection.vue
@@ -1,11 +1,51 @@
 <template>
   <section class="container how-to-donate" tabindex="0">
     <div class="how-to-donate__content">
-      <h1 class="section__title">Como doar</h1>
+      <h1 class="section__title how-to-donate__title">Como doar</h1>
       <swiper class="swiper" :options="swiperOption">
-        <swiper-slide><Slide1></Slide1></swiper-slide>
-        <swiper-slide><Slide1></Slide1></swiper-slide>
-        <swiper-slide><Slide1></Slide1></swiper-slide>
+        <swiper-slide>
+          <Slide>
+            <h1 slot="title">Verifique</h1>
+            <img
+              slot="background"
+              class="slide__background"
+              src="../../assets/slide-1-background.png"
+              alt="section_background"
+              role="presentation"
+            />
+            <p slot="description">
+              Tem um telemóvel ou tablet com câmara, carregador e em boas
+              condições ?
+            </p>
+            <p slot="notes">
+              Independentemente da marca, o telemóvel terá que estar funcional,
+              ter câmaras para video conferência e ter um carregador funcional.
+              Certifique-se que limpa todos os dados pessoais do mesmo.
+            </p>
+          </Slide>
+        </swiper-slide>
+        <swiper-slide>
+          <Slide link="#" link-text="Encontre os pontos de recolha">
+            <h1 slot="title">Entregue num ponto recolha à sua escolha</h1>
+            <p slot="description">
+              Existem mais de 17 pontos de recolha, de norte a sul.
+            </p>
+          </Slide>
+        </swiper-slide>
+        <swiper-slide>
+          <Slide>
+            <h1 slot="title">Ajude quem mais precisa neste momento.</h1>
+            <p slot="description">
+              As instituições vão certificar-se que a sua doação irá chegar a
+              quem mais precisa.
+            </p>
+            <p slot="notes">
+              Existem inúmeras pessoas isoladas neste momento, em lares,
+              hospitais e outras instituições, sem meios de comunicarem com as
+              famílias.
+            </p>
+          </Slide>
+        </swiper-slide>
         <div slot="pagination" class="swiper__pagination"></div>
         <div slot="button-prev" class="swiper__buttonPrev"></div>
         <div slot="button-next" class="swiper__buttonNext"></div>
@@ -16,7 +56,7 @@
 
 <script>
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-import Slide1 from './Slides/Slide1'
+import Slide from './Slides/Slide'
 import 'swiper/css/swiper.css'
 
 export default {
@@ -24,7 +64,7 @@ export default {
   components: {
     Swiper,
     SwiperSlide,
-    Slide1
+    Slide
   },
   data() {
     return {
@@ -51,9 +91,11 @@ export default {
 
 .how-to-donate {
   height: 100vh;
-
-  &__content {
+  &__title {
     text-align: center;
+    margin-bottom: 47px;
+  }
+  &__content {
     grid-column: 1 / 13;
     width: 100%;
   }
@@ -64,6 +106,7 @@ export default {
   width: 100%;
 
   &__pagination {
+    text-align: center;
     padding-top: 10px;
   }
 }
---
+++ components/HowToDonateSection/Slides/Slide.vue
@@ -0,0 +1,93 @@
+<template>
+  <section class="container slide" tabindex="0">
+    <div class="slide__background">
+      <slot name="background" />
+    </div>
+    <div class="slide__info">
+      <div class="section__title slide__infoTitle"><slot name="title" /></div>
+      <div class="section__description slide__infoDescription">
+        <slot name="description" />
+      </div>
+      <div class="section__description slide__infoNotes">
+        <slot name="notes" />
+      </div>
+      <Button
+        v-if="link"
+        class="slide__infoLink"
+        :link="link"
+        kind="tertiary"
+        >{{ linkText }}</Button
+      >
+    </div>
+  </section>
+</template>
+
+<script>
+import Button from '~/components/Button/Button'
+export default {
+  name: 'Slide',
+  components: {
+    Button
+  },
+  props: {
+    link: {
+      type: String,
+      default: undefined
+    },
+    linkText: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../styles/_global.scss';
+
+.slide {
+  height: 80vh;
+
+  &__background {
+    max-width: 100%;
+    grid-column: 1 / 13;
+    @media screen and (min-width: $max-mobile-size) {
+      grid-column: 1 / 7;
+    }
+  }
+
+  &__info {
+    font-family: Roboto, sans-serif;
+    grid-column: 1 / 13;
+    @media screen and (min-width: $max-mobile-size) {
+      grid-column: 8 / 13;
+    }
+  }
+
+  &__infoTitle {
+    font-family: Muli, sans-serif;
+    font-weight: 900;
+    font-size: 32px;
+    line-height: 40px;
+    color: $primary-color;
+  }
+
+  &__infoDescription {
+    font-size: 24px;
+    line-height: 38px;
+    color: $black;
+    margin-top: 24px;
+  }
+
+  &__infoNotes {
+    font-size: 16px;
+    line-height: 30px;
+    color: $gray;
+    margin-top: 24px;
+  }
+
+  &__infoLink {
+    width: max-content;
+  }
+}
+</style>

98ed4394c5f475215c1d9f65ffa49a8aa3c21369
Tiago Rechau <tiago.rechau@mercedes-benz.io>
2020-04-14 21:42:21 +0100

    Collecting stations list

---
+++ components/CollectingStationsSection/List/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="list__container">
+    <LocationCard
+      v-for="item in items"
+      :key="item.name"
+      :name="item.name"
+      :location="item.location"
+      :working-hours="item.workingHours"
+      :url="item.url"
+    />
+  </div>
+</template>
+
+<script>
+import LocationCard from '~/components/LocationCard/LocationCard'
+export default {
+  name: 'CollectionStationsList',
+  components: {
+    LocationCard
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.list__container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 32px;
+}
+</style>
---
+++ components/LocationCard/LocationCard.vue
@@ -0,0 +1,89 @@
+<template>
+  <article class="list-item__container">
+    <h1 class="list-item__headline">{{ name }}</h1>
+    <span class="list-item__info">
+      <span class="list-item__info__icon"><MapMarker /></span>
+      {{ location }}
+    </span>
+    <span class="list-item__info">
+      <span class="list-item__info__icon"><ClockOutline /></span>
+      {{ workingHours }}
+    </span>
+    <Button
+      class="list-item__link"
+      kind="tertiary"
+      :href="url"
+      target="__blank"
+      align="left"
+    >
+      abrir no google maps
+    </Button>
+  </article>
+</template>
+
+<script>
+import MapMarker from 'vue-material-design-icons/MapMarker.vue'
+import ClockOutline from 'vue-material-design-icons/ClockOutline.vue'
+import Button from '~/components/Button/Button'
+export default {
+  name: 'CollectionStationsListItem',
+  components: {
+    Button,
+    MapMarker,
+    ClockOutline
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    workingHours: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/_global';
+.list-item {
+  &__container {
+    display: grid;
+    grid-gap: 8px;
+    padding: 16px 22px;
+    border: 2px solid $white;
+    box-sizing: border-box;
+    border-radius: 8px;
+  }
+
+  &__headline {
+    font-family: Muli, sans-serif;
+    font-weight: 900;
+    font-size: 20px;
+    line-height: 25px;
+  }
+
+  &__info {
+    display: flex;
+    align-items: center;
+    font-family: Roboto, sans-serif;
+    font-size: 16px;
+    line-height: 30px;
+    color: $gray;
+
+    &__icon {
+      margin-right: 8px;
+      color: $primary-color;
+    }
+  }
+}
+</style>

dcd9946c2565be18eed51b62dca24881d8cf15e7
Pedro Gomes <pedrompg311@gmail.com>
2020-04-20 14:46:26 +0100

    Small changes according to design

---
+++ components/TemporaryMessage/TemporaryMessage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    v-if="showMessage"
+    class="temporary-message"
+    @click="showMessage = false"
+  >
+    {{ temporaryMessage }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TemporaryMessage',
+  props: {
+    temporaryMessage: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      showMessage: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../styles/_global';
+
+.temporary-message {
+  font-family: 'Muli', sans-serif;
+  width: 100vw;
+  font-size: 15px;
+  padding: 15px;
+  text-align: center;
+  color: $white;
+  background-color: $primary-color;
+}
+</style>

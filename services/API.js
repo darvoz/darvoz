c835ed8862b70165f904d962f3cabc725102d1c5
Tiago Rechau <tiago.rechau@mercedes-benz.io>
2020-05-01 03:10:36 +0100

    Update API and removed firebase

---
+++ services/API.js
@@ -0,0 +1,23 @@
+const blobToBase64 = (file) => {
+  const reader = new FileReader()
+  reader.readAsDataURL(file)
+  return new Promise((resolve) => {
+    reader.onloadend = () => {
+      resolve(reader.result.replace(/^data:.+;base64,/, ''))
+    }
+  })
+}
+const API_ENDPOINT =
+  'https://limitless-journey-69923.herokuapp.com/api/darvoz/upload'
+
+export const uploadFile = (file, { parish, streetNumber, street }) => {
+  return blobToBase64(file).then((content) => {
+    return fetch(API_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ content, parish, streetNumber, street })
+    })
+  })
+}

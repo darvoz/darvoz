d9735567c933d960e1e433fb434da3ef9030d80e
Tiago Rechau <tiago.rechau@mercedes-benz.io>
2020-05-29 17:59:05 +0100

    Santos Populares iniatives design

---
+++ components/IntroSection/SantosIntroSection.vue
@@ -0,0 +1,83 @@
+<template>
+  <GenericIntroSection
+    :is-first-section="isFirstSection"
+    class="santos-intro__info"
+    :primary-cta="{
+      link: '#quadra',
+      label: localI18n['darvoz-santos.intro.cta']
+    }"
+  >
+    <div slot="background" class="santos-intro__imageWrapper">
+      <picture slot="background">
+        <img
+          src="../../assets/santos/santos__introBackground.png"
+          alt="section_background"
+          class="intro-section__backgroundImg santos-intro__backgroundImg"
+        />
+      </picture>
+    </div>
+    <template slot="headline">
+      {{ localI18n['darvoz-santos.intro.headline.normal'] }}
+      <span class="intro-section__infoTitle--strong">
+        {{ localI18n['darvoz-santos.intro.headline.strong'] }}
+      </span>
+    </template>
+    <template slot="description">
+      {{ localI18n['darvoz-carris.intro.description'] }}
+    </template>
+  </GenericIntroSection>
+</template>
+
+<script>
+import localI18n from '../../data/resources/i18n'
+import GenericIntroSection from './GenericIntroSection'
+
+export default {
+  name: 'SantosIntroSection',
+  components: {
+    GenericIntroSection
+  },
+  props: {
+    isFirstSection: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return { localI18n }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../styles/_global.scss';
+
+.santos-intro {
+  &__info {
+    background-color: $carris-yellow;
+  }
+
+  &__imageWrapper {
+    display: flex;
+    justify-content: center;
+  }
+
+  &__backgroundImg {
+    object-fit: contain;
+    width: 100%;
+    max-width: 100%;
+    height: 400px;
+  }
+
+  @media only screen and (min-width: $max-mobile-size) {
+    &__backgroundImg {
+      width: 669px;
+      height: 610px;
+    }
+
+    &__imageWrapper {
+      justify-content: flex-end;
+    }
+  }
+}
+</style>
